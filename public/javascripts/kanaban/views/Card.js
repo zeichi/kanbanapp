5a9d86182fdab1884f4981c917a9d81cecabf1d7
zeichi <zeichi@naver.com>
2017-11-21 08:54:32 -0800

    Card class의 title 속성에 대한 커스텀 propTypes 유효성 검사기 정의 및 추가

--- public/javascripts/kanaban/views/Card.js
+++ public/javascripts/kanaban/views/Card.js
@@ -5,6 +5,17 @@ import React, {Component, PropTypes} from 'react';
 import CheckList from './CheckList';
 import marked from 'marked';
 
+let titlePropType = (proprs, propName, componentName) => {
+    if (props[propName]) {
+        let value = props[propName];
+        if (typeof value !== 'string' || value.length > 80) {
+            return new Error(
+                '${propName} in &{componentName} is longer than 80 characters'
+            );
+        }
+    }
+};
+
 class Card extends Component {
     constructor() {
         super(...arguments);
@@ -66,7 +77,7 @@ class Card extends Component {
 
 Card.propTypes = {
     id: PropTypes.number,
-    title: PropTypes.string,
+    title: titlePropType,
     description: PropTypes.string,
     color: PropTypes.string,
     tasks: PropTypes.arrayOf(PropTypes.object)
