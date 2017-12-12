f2a4215f3f1449c885fad8ba0fbc161547130ec4
zeichi <zeichi@naver.com>
2017-12-12 09:11:45 -0800

    package.json 라이브러리 추가 및 build 스크립트 수정

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,22 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: './app.js',
+    entry: './bin/www',
     output: {
+        // __dirname : 현재 실행중인 스크립트가 있는 디렉터리 이름을 포함하는 node.js 전역 변수
         path: path.resolve(__dirname, 'dist'),
         // publicPath: '/dist/',
         filename: 'bundle.js'
     },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
     module: {
         loaders: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 loader: 'babel-loader',
+                include: path.resolve(__dirname, './'),
                 exclude: /node_modules/,
                 query: {
                     cacheDirectory: true,
