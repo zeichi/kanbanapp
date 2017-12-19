cd08889632e6707107c475410f0a7db87437aaf0
zeichi <zeichi@naver.com>
2017-12-19 14:29:48 -0800

    webpack.config 설정 변경 - entry 및 output 내용 변경 - express 번들링 하지않도록 수정

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,29 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: './bin/www',
-    output: {
-        // __dirname : 현재 실행중인 스크립트가 있는 디렉터리 이름을 포함하는 node.js 전역 변수
-        path: path.resolve(__dirname, 'dist'),
-        // publicPath: '/dist/',
-        filename: 'bundle.js'
+    entry: {
+        bundle: "./public/javascripts/main.js",
+        // server: "./server/main.js"
     },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].js',
+    },
+
+    // entry: [/*"./bin/www"*/'./app.js'/*, './public/javascript/kanaban/main.js'*/],
+    // output: {
+        // __dirname : 현재 실행중인 스크립트가 있는 디렉터리 이름을 포함하는 node.js 전역 변수
+        // path: path.resolve(__dirname, 'dist'),
+        // publicPath: '/dist/',
+        // filename: 'bundle.js'
+    // },
     resolve: {
         extensions: ['.js', '.jsx']
     },
     module: {
         loaders: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.jsx?$/,
                 loader: 'babel-loader',
                 include: path.resolve(__dirname, './'),
                 exclude: /node_modules/,
